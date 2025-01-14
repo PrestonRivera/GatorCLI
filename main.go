ce0d4f5e8c237f8ba2f7a3193d8d0f272d466da2
PrestonRivera <prestonrivera211@gmail.com>
2025-01-13 23:44:00 -0600

    feat: Add SQLC integration and user authentication
    - Set up SQLC configuration and PostgreSQL driver
    - Implement user registration with UUID generation
    - Add login verification against database
    - Create database queries for user operations

--- main.go
+++ main.go
@@ -4,30 +4,45 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"database/sql"
 	"github.com/PrestonRivera/GatorCLI/internal/config"
+	"github.com/PrestonRivera/GatorCLI/internal/database"
+	_ "github.com/lib/pq"
 )
 
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
+const dbURL = "postgres://postgres:postgres@localhost:5432/gator"
 
 func main() {
-	cfg, err := config.Read()
+		cfg, err := config.Read()
 	if err != nil {
 		fmt.Println("Error reading config: ", err)
 		return 
 	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Set up for db connection and queries failed")
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db: dbQueries,
 		cfg: &cfg,
 	}
 
-	cmds := &commands{
+	cmds := commands{
 		cliCommands: make(map[string]func(*state, command) error),
 	}
 
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 	
 	if len(os.Args) < 2 {
 		log.Fatal("Not enough arguments were provided")
